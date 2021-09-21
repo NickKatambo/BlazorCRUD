fca20c45db3220c3bd13af22db0f22e9106f3d06
NickKatambo <yannick.katambo@outlook.com>
2021-09-21 15:38:45 +0200

    Done with Earning module. Added Expense module.

---
+++ FinanceMentor/Shared/ExpenseCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceMentor.Shared
+{
+    public enum ExpenseCategory
+    {
+        Grocery,
+        Insurance,
+        Clothing,
+        Healthcare,
+        Debt,
+        Gifts,
+        Savings,
+        Transportation,
+        Entertainment,
+        Fuel,
+        Rent,
+        School,
+        Utilities
+    }
+}
